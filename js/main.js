a492fdfe3d2e035c606f4f3b502159c2d536d3c1
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2024-06-21 19:14:36 +0530

    new design update

--- js/main.js
+++ js/main.js
@@ -21,10 +21,10 @@
                 items: 1
             },
             600: {
-                items: 2
+                items: 2.5
             },
             1000: {
-                items: 4
+                items: 5
             }
         }
     })
