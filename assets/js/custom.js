70929b9ba1e175e916c1bd039ca156f00ac67f22
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2025-02-07 16:30:45 +0530

    time line journey section updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,28 @@
-/* ==================================================
-    # Carousel Slide For Services
- ===============================================*/
- $('.carousel-service-items').owlCarousel({
-    loop: false,
-    nav: false,
-    dots: true,
-    items: 1,
-    navText: [
-        "<i class='fa fa-angle-left'></i>",
-        "<i class='fa fa-angle-right'></i>"
-    ],
-})
+
+(function($) {
+    "use strict";
+    $(document).on('ready', function() {
+
+    /* ========== # Carousel Slide For Services Section =========== */
+    $('.carousel-service-items').owlCarousel({
+        loop: false,
+        nav: false,
+        dots: true,
+        items: 1,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+    })
+
+    /* ========== # Carousel Slide For Journey Section =========== */
+    timeline(document.querySelectorAll('.timeline'), {
+        forceVerticalMode: 991,
+        mode: 'horizontal',
+        verticalStartPosition: 'left',
+        visibleItems: 3
+    });
+
+
+    });
+})(jQuery);
