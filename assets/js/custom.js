323b964bfbdd9fe22c7595818376fc86be6480fa
Chaitanya Kothakota <kcbgangadhar@gmail.com>
2025-02-07 14:23:43 +0530

    services slider section updated

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+/* ==================================================
+    # Carousel Slide For Services
+ ===============================================*/
+ $('.carousel-service-items').owlCarousel({
+    loop: false,
+    nav: false,
+    dots: true,
+    items: 1,
+    navText: [
+        "<i class='fa fa-angle-left'></i>",
+        "<i class='fa fa-angle-right'></i>"
+    ],
+})
